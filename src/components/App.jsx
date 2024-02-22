93a8cf1437b33f5066553762b935fe8ddfcd4186
Andrii2024 <krovlya2005@gmail.com>
2024-02-22 09:09:23 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,39 @@
-import React from 'react';
-import { Component } from 'react';
+import React, { useState } from 'react';
+// import { Component } from 'react';
 import FeedbackApp from './Feedback/FeedbackApp';
 import Statistics from './Statistic/Statistics';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 import styled from 'styled-components';
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const handleLeaveFeedback = feedbackType => {
+    setGood(prevGood => prevGood + 1);
+    setNeutral(prevNeutral => prevNeutral + 1);
+    setBad(prevBad => prevBad + 1);
+    // this.setState(prevState => ({
+    //   [feedbackType]: prevState[feedbackType] + 1,
+    // }));
   };
-
-  handleLeaveFeedback = feedbackType => {
-    this.setState(prevState => ({
-      [feedbackType]: prevState[feedbackType] + 1,
-    }));
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-
-    return (
-      <StyledWrapper>
-        <h1>Please leave feedback</h1>
-        <FeedbackApp onLeaveFeedback={this.handleLeaveFeedback} />
-        {total === 0 ? (
-          <Notification message="There is no feedback" />
-        ) : (
-          <Section title="">
-            <Statistics good={good} neutral={neutral} bad={bad} />
-          </Section>
-        )}
-      </StyledWrapper>
-    );
-  }
-}
-
+  const total = good + neutral + bad;
+  return (
+    <StyledWrapper>
+      <h1>Please leave feedback</h1>
+      <FeedbackApp onLeaveFeedback={handleLeaveFeedback} />
+      {total === 0 ? (
+        <Notification message="There is no feedback" />
+      ) : (
+        <Section title="">
+          <Statistics good={good} neutral={neutral} bad={bad} />
+        </Section>
+      )}
+    </StyledWrapper>
+  );
+};
 export default App;
-
 export const StyledWrapper = styled.div`
   border-radius: 10px;
   margin: 0 auto;
@@ -52,3 +45,35 @@ export const StyledWrapper = styled.div`
   flex-direction: column;
   gap: 10px;
 `;
+// class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   handleLeaveFeedback = feedbackType => {
+//     this.setState(prevState => ({
+//       [feedbackType]: prevState[feedbackType] + 1,
+//     }));
+//   };
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const total = good + neutral + bad;
+
+//     return (
+//       <StyledWrapper>
+//         <h1>Please leave feedback</h1>
+//         <FeedbackApp onLeaveFeedback={this.handleLeaveFeedback} />
+//         {total === 0 ? (
+//           <Notification message="There is no feedback" />
+//         ) : (
+//           <Section title="">
+//             <Statistics good={good} neutral={neutral} bad={bad} />
+//           </Section>
+//         )}
+//       </StyledWrapper>
+//     );
+//   }
+// }
